f7f7fe451723805e034539610478a6439271152f
0xMichaelRan <michaelran16@gmail.com>
2024-11-06 18:28:20 +0800

    update README

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ app = FastAPI()
 @app.get("/") 
 async def main_route():     
   return {"message": "Hey, this is 0xMichaelRanr"}
+
+# Reference: https://medium.com/@caetanoog/start-your-first-fastapi-server-with-poetry-in-10-minutes-fef90e9604d9
